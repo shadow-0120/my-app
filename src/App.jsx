1d374086b95b5b52fdd3fa75e56e13a1c0346c0a
shadow-0120 <benweidz33@gmail.com>
2025-05-10 17:59:13 +0100

    Header Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import Header from './components/header/header';
+import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skilles/Skills';
